80f059ef6acf2a8098f4f823cf42c15ca0122cba
Nguyen Van Anh <nguyenvananh.connect@gmail.com>
2025-11-12 22:45:29 +0700

    hotfix add debug

--- app/toolbar-editor-v1/page.tsx
+++ app/toolbar-editor-v1/page.tsx
@@ -183,6 +183,8 @@ function ToolbarEditorContent() {
     // Lắng nghe message từ iframe cha
     const handleMessage = (event: MessageEvent) => {
       if (event.data.type === 'SET_INITIAL_CONTENT') {
+        console.log('DEBUG _ SET INIT CONTENT', event.data.content);
+        
         if (isReadonly) {
           setContent(event.data.content)
           setTimeout(() => {
